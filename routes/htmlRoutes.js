27df0b0fd449b4655aaf79dffe86ed4db05ed6c4
Kendall <ktrudickm@gmail.com>
2021-05-17 19:37:21 -0400

    Completed html Routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,7 +1,16 @@
 const path = require("path");
-const express = require("express");
 
 
 module.exports = function (app) {
-    
+    app.get("/exercise", (req, res) => {
+        res.sendFile(path.join(_dirname, "../public/exercise.html"));
+    });
+
+    app.get("/", (req, res) => {
+        res.sendFile(path.join(_dirname, "../public/index.html"));
+    });
+
+    app.get("/stats", (req, res) => {
+        res.sendFile(path.join(_dirname, "../public/stats.html"));
+    });
 }
